a5d0b3cf6240cc01b99c803c5ae2654bcd93a2ca
UmangJ3 <haharofl14@gmail.com>
2024-07-28 23:23:53 +0530

    Create color_tests.h

---
+++ color_tests.h
@@ -0,0 +1,7 @@
+#ifndef COLOR_TESTS_H
+#define COLOR_TESTS_H
+
+void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
+void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
+
+#endif 
