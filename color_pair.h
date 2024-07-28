3a767796c254b326539ea46177380aa68567a7d3
UmangJ3 <haharofl14@gmail.com>
2024-07-28 23:19:08 +0530

    Create color_pair.h

---
+++ color_pair.h
@@ -0,0 +1,19 @@
+#ifndef COLOR_PAIR_H
+#define COLOR_PAIR_H
+
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+extern const char* MajorColorNames[];
+extern int numberOfMajorColors;
+extern const char* MinorColorNames[];
+extern int numberOfMinorColors;
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
+
+#endif
